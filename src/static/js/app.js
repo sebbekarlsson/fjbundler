58692c6c5c8ea86d9cf2b8f8e8cee5cf03cb755f
ianertson <sebbekarlsson97@gmail.com>
2021-02-14 07:48:51 +0100

    add cookie banner

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,5 +1,9 @@
 import { initializeNav } from './nav';
 import { initializeTerminal } from './terminal/init';
+import { CookieBanner } from './cookiebanner.min.js';
+
+const options = { message: "We use cookies to enhance your experience. If you continue to use this website, you consent.", moreinfo: "https://gdpr.eu/cookies/" };
+const cb = new Cookiebanner(options); cb.run();
 
 
 initializeNav();
